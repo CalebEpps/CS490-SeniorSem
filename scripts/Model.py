d30aba95b43b910193e6964d918242fcfb295958
SingtheC <caleb.epps@yahoo.com>
2023-04-03 11:24:24 -0400

    - Committed Pretrained model for use with Android Studio - Added save method to model for easy saving - Added validation method - Added Accuracy tracking to the validation method - Tweaked some values in order to achieve more stable loss results. - Current accuracy clocking in at 94%

--- scripts/Model.py
+++ scripts/Model.py
@@ -11,7 +11,6 @@ class FashionMNISTModel(nn.Module):
         super(FashionMNISTModel, self).__init__()
 
         self.cl_1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
-        self.m_pool = nn.MaxPool2d(2, 2)
         self.cl_2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
 
         self.linear_1 = nn.Linear(in_features=(12 * 4 * 4), out_features=120)
@@ -25,7 +24,6 @@ class FashionMNISTModel(nn.Module):
         # do convolution 1
         in_item = self.cl_1(in_item)
         in_item = relu(in_item)
-        # *|CURSOR_MARCADOR|*
         in_item = max_pool2d(in_item, kernel_size=2, stride=2)
 
 
@@ -35,7 +33,7 @@ class FashionMNISTModel(nn.Module):
         in_item = max_pool2d(in_item, kernel_size=2, stride=2)
 
         # reshape for Linear
-        in_item = in_item.reshape(-1, 12 * 4 * 4)
+        in_item = torch.flatten(in_item, start_dim=1)
         
         # Do Linear 1
         in_item = self.linear_1(in_item)
