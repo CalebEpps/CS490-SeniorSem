d659df68ddbfc92bfac7e52b7c32f0b4c9dcc529
SingtheC <caleb.epps@yahoo.com>
2023-04-03 08:43:57 -0400

    - Fix for missing Python scripts

---
+++ scripts/Logger.py
@@ -0,0 +1,11 @@
+class Training_Logger:
+    def __init__(self):
+        pass
+        # TODO:
+        #Track epoch, loss, count, accuracy, and time.
+        #Track for tensorboard here, send images and graphs to TB
+        # Add functions for when the run ends, when epochs end, and when epochs start.
+        # Add trackers for correct predictions
+        # Maybe save to file?
+
+
---
+++ scripts/Model.py
@@ -0,0 +1,56 @@
+import torch.cuda
+import torch.nn as nn
+from torch import relu, max_pool2d
+
+from torch.utils.data import Dataset, DataLoader
+
+
+class FashionMNISTModel(nn.Module):
+    def __init__(self):
+        # Init parent const
+        super(FashionMNISTModel, self).__init__()
+
+        self.cl_1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.m_pool = nn.MaxPool2d(2, 2)
+        self.cl_2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
+
+        self.linear_1 = nn.Linear(in_features=(12 * 4 * 4), out_features=120)
+        self.linear_2 = nn.Linear(in_features=120, out_features=60)
+
+        self.out = nn.Linear(in_features=60, out_features=10)
+
+
+    def forward(self, in_item):
+
+        # do convolution 1
+        in_item = self.cl_1(in_item)
+        in_item = relu(in_item)
+        # *|CURSOR_MARCADOR|*
+        in_item = max_pool2d(in_item, kernel_size=2, stride=2)
+
+
+        # Do convolution 2
+        in_item = self.cl_2(in_item)
+        in_item = relu(in_item)
+        in_item = max_pool2d(in_item, kernel_size=2, stride=2)
+
+        # reshape for Linear
+        in_item = in_item.reshape(-1, 12 * 4 * 4)
+        
+        # Do Linear 1
+        in_item = self.linear_1(in_item)
+        in_item = relu(in_item)
+
+        # Do Linear 2
+        in_item = self.linear_2(in_item)
+        in_item = relu(in_item)
+
+        in_item = self.out(in_item)
+        return in_item
+    
+
+
+
+
+
+
