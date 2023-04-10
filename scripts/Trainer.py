ced4d27ae892e13e0cea4ead21ab89b04643062e
SingtheC <caleb.epps@yahoo.com>
2023-04-10 12:03:15 -0400

    - Wrote test loop. Numbers are showing slightly off currently, but model is being tested properly.

--- scripts/Trainer.py
+++ scripts/Trainer.py
@@ -76,32 +76,30 @@ class FashionTrainer:
         # Begin Epoch Run
         for epoch in range(self.epochs):
 
-            self.model.train(True)
+            self.model.eval()
             # reset current loss to 0 for next training iteration
             curr_loss = 0.0
             correct = 0
             # iterator
             count = 0
-            for img, label in self.loader.validation_loader:
-                # Send image and labels to device.
-                img = img.to(self.device)
-                label = label.to(self.device)
-                output = self.model(img)
-                loss = self.crit(output, label)
+            inner_count = 0
+            with torch.no_grad():
+                for img, label in self.loader.validation_loader:
+                    # Send image and labels to device.
+                    inner_count += 1
+                    img = img.to(self.device)
+                    label = label.to(self.device)
+                    output = self.model(img)
+                    loss = self.crit(output, label)
 
-                curr_loss = loss.item()
+                    curr_loss = curr_loss + loss.item()
 
-                correct += self.get_correct_preds(output, label)
-                # Write to TB
-                self.writer.add_scalar("Accuracy", correct / len(self.loader.training_set), epoch)
-                self.writer.add_scalar('Training Loss', curr_loss, global_step=epoch)
+                    correct += self.get_correct_preds(output, label)
+                    # Write to TB
+                    self.writer.filename_suffix = "Validate"
+                    self.writer.add_scalar("Accuracy", correct / len(self.loader.training_set), epoch)
+                    self.writer.add_scalar('Training Loss', curr_loss / inner_count, global_step=epoch)
 
-                # Set gradient
-                self.opt.zero_grad()
-                loss.backward()
-
-                self.opt.step()
-                # Calculate current loss
 
             # Update Total Loss
             total_loss += curr_loss / (count + 1)
@@ -111,7 +109,7 @@ class FashionTrainer:
             print("Epoch: ", epoch)
             print("Total Loss: ", total_loss)
             print("Current Loss: ", curr_loss)
-            print("Accuracy: ", (correct / len(self.loader.training_set)))
+            print("Accuracy: ", (correct / len(self.loader.validation_loader)))
             print("\n")
 
         self.writer.close()
@@ -145,5 +143,5 @@ class FashionTrainer:
 
 
 if __name__ == "__main__":
-    trainer = FashionTrainer(lr=0.001, epochs=100)
-    trainer.train()
+    trainer = FashionTrainer(lr=0.001, epochs=50)
+    trainer.validate()
