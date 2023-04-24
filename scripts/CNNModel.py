3a58a4ca10be95aa94f68f7a199305a39f3f5617
SingtheC <caleb.epps@yahoo.com>
2023-04-24 11:57:20 -0400

    - Added missing comments to the CNNModel.py File

--- scripts/CNNModel.py
+++ scripts/CNNModel.py
@@ -9,10 +9,10 @@ class FashionMNISTModel(nn.Module):
     def __init__(self):
         # Init parent const
         super(FashionMNISTModel, self).__init__()
-
+        # Initialize 2 Convolutional Layers
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
-
+        # Initialize 2 Linear Layers
         self.fc1 = nn.Linear(in_features=(12 * 4 * 4), out_features=120)
         self.fc2 = nn.Linear(in_features=120, out_features=60)
 
