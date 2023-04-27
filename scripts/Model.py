7bd369bb890465c56060195464b308debea2dbe8
k3ny3tta <89467222+K3ny3tta@users.noreply.github.com>
2023-04-27 09:56:28 -0400

    Update Model.py

--- scripts/Model.py
+++ scripts/Model.py
@@ -2,6 +2,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+"""
+An NNM Neural Network Model for imageclassification using the MNISTDATA SET
+
+Attributes:
+self.conv1 (nn.Conv2d) First convolutional layer with three input channels and 6 output
+self.pool(nn.MaxPool2d) Max pooling with size of 2 by 2 
+self.conv2(nnConv2d) second convolutional layer with 6 input channels and 16 output
+self.fc1(nn.Linear) connected layer with 16x4x4 input features and 120 output
+self.fc2(nn.Linear) connected layer with 120 input features and 84 output
+self.fc3(nn.Linear) connected layer with 84 input features and 10 output
+
+methods:
+__init_(self) initializes NNM layers
+forward(self,x) passses the NN
+
+Return:
+None
+
+"""
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -12,6 +32,16 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+        
+        """
+        Passes the NN (Neural Network)
+        
+        Args:
+        x(torch.flatten) input tensor shape 
+        
+        Returns: 
+        Produces the shape of the tensor with batch size
+        """
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
