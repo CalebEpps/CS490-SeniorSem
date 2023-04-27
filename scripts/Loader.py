6a5d90be77faaff3b00a20ccd9594d68b079eef5
k3ny3tta <89467222+K3ny3tta@users.noreply.github.com>
2023-04-26 21:46:49 -0400

    Update Loader.py
    Added docstrings

--- scripts/Loader.py
+++ scripts/Loader.py
@@ -5,6 +5,29 @@ from torchvision.datasets import FashionMNIST
 
 
 class FashionLoader(Dataset):
+    
+    """
+    intializes a dataset for training a nerual network on FashionMNIST dataset.
+    
+    Args: 
+         parameter1: batch_size(int) the amount of samples in each batch to load.
+         
+    Attributes: 
+        batch_size (int) batch size for the loading data 
+        self.test_dataset The entire FashionMNIST dataset that is used for training
+        len_dataset This dataset is used for testing 
+        len_training_set training subset for entire dataset 
+        len_validation_set the validation of the dataset 
+        self.training_loader dataloader that is for loading the training data
+        self.validation_loader the dataloader for loadign the validation set
+        self.test_loader datatloader for loading test data
+        self.classes list of classes with the names of the 10 item types 
+        
+        Returns:
+        NULL
+        
+        """
+        
     ### Initialize dataset class, inherits parent
     def __init__(self, batch_size=128):
         self.batch_size = batch_size
@@ -40,4 +63,15 @@ class FashionLoader(Dataset):
 
     # This is a simple debug method and is obsolete.
     def training_set_length(self):
+        """
+        this returns the amount of samplesin the training set of the data
+        
+        Args:
+            None
+            
+            Returns:
+            int amount of samples in the training dataset
+                
+        """
+        
         return len(self.training_set)
